ad64a17da015109397df2b71babf2792368a3d3e
Seivan Heidari <seivan@kth.se>
2013-03-27 11:28:44 +0100

    Podspec mess

--- Example/SHAccountStoreExample/SHViewController.m
+++ Example/SHAccountStoreExample/SHViewController.m
@@ -14,16 +14,8 @@
 
 @implementation SHViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+-(void)viewDidAppear:(BOOL)animated; {
+  [super viewDidAppear:animated];
+  
 }
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 @end
